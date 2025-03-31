d4c4f4255b12065ff20b8d00c66f2669cb0a3c92
Amir <109381498+Amir-Ramez@users.noreply.github.com>
2025-03-31 17:05:52 +0200

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+    if (typeof first != typeof second) return 'ERROR';
+    if (!(first >= 0 && second >= 0)) return 'ERROR';
+    if (Math.floor(first) < first || Math.floor(second) < second)
+        return 'ERROR';
+    if (second < first) return sumAll(second, first);
+    return (second * (second + 1) - first * (first - 1)) / 2;
 };
 
 // Do not edit below this line
