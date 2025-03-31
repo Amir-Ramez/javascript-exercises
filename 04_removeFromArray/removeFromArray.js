bd68a3d4ed71bef747fd2421047c2d386d00da41
Amir <109381498+Amir-Ramez@users.noreply.github.com>
2025-03-31 15:50:45 +0200

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...target) {
+    for (let i = 0; i < target.length; ++i) {
+        arr = arr.filter((item) => item !== target[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
