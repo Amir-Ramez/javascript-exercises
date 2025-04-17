279afb5c7c21503bbb33ac8707e7c2483bba9b9e
Amir <109381498+Amir-Ramez@users.noreply.github.com>
2025-04-17 21:52:01 +0200

    Solve exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    str = str
+        .toLowerCase()
+        .split('')
+        .filter((chr) => chars.includes(chr))
+        .join('');
 
+    for (let i = 0; i < str.length / 2; ++i)
+        if (str.charAt(i) !== str.charAt(str.length - i - 1)) return false;
+    return true;
 };
 
 // Do not edit below this line
