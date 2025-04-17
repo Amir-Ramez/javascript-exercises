29888d695a47672c6f9a2238afea308c6a92b82e
Amir <109381498+Amir-Ramez@users.noreply.github.com>
2025-04-17 22:53:41 +0200

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    const oldest = arr.reduce((previous, current) => {
+        const previousAge = getAge(previous.yearOfBirth, previous.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return currentAge > previousAge ? current : previous;
+    });
+    return oldest;
 };
+
+function getAge(yearOfBirth, yearOfDeath) {
+    yearOfDeath = yearOfDeath || new Date().getFullYear();
+    return yearOfDeath - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
